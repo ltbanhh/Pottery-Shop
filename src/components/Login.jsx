bafef972bb2e4f994319c7d9b298150defab9185
ltbanhh <lethanhbach2004@gmail.com>
2025-04-10 08:41:02 +0700

    update login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export function Login() {
   const navigate = useNavigate()
@@ -23,9 +24,9 @@ export function Login() {
           <p className="text-xs mt-4 text-[#894A2A]">If you are already a member, easily log in</p>
 
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <input className="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email" required/>
+            <input className="p-2 mt-8 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="email" name="email" placeholder="Email" required/>
             <div className="relative">
-              <input className="p-2 rounded-xl border w-full" type="password" name="password" placeholder="Password" required minLength={6}/>
+              <input className="p-2 w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="password" name="password" placeholder="Password" required minLength={6}/>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                 <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
@@ -56,7 +57,9 @@ export function Login() {
 
           <div className="mt-3 text-xs flex justify-between items-center text-[#894A2A]">
             <p>Don't have an account?</p>
-            <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Register</button>
+            <Link to="/signup">
+              <button type='button' className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Register</button>
+            </Link>
           </div>
         </div>
 
