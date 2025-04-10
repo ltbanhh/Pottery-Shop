7da4a32dd59555019e644c06b92a9a28643eec4a
ltbanhh <lethanhbach2004@gmail.com>
2025-04-11 01:10:09 +0700

    update login signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,15 +22,15 @@ export function Login() {
   }
 
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <div className="bg-white flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+    <div className="bg-gradient-to-r from-[#F8DBA4] to-[#E09082] min-h-screen flex items-center justify-center">
+      <div className="bg-white/50 backdrop-blur-3xl ring-1 ring-black/5 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
         <div className="md:w-1/2 px-8 md:px-8">
           <h2 className="font-bold text-2xl text-[#894A2A]">Login</h2>
           <p className="text-xs mt-4 text-[#894A2A]">If you are already a member, easily log in</p>
 
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <input 
-              className="p-2 mt-8 rounded-md bg-white px-3 py-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" 
+              className="mt-8 rounded-md px-3 py-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-[#894A2A] placeholder:text-[#894A2A] focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" 
               type="email" 
               name="email" 
               placeholder="Email" 
@@ -38,38 +38,40 @@ export function Login() {
             />
             <div className="relative">
               <input 
-                className="p-2 w-full rounded-md bg-white px-3 py-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" 
+                className="w-full rounded-md px-3 pr-12 py-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-[#894A2A] placeholder:text-[#894A2A] focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" 
                 type={showPassword ? "text" : "password"}
                 name="password" 
                 placeholder="Password" 
                 required 
                 minLength={6}
               />
-                <button type="button" onClick={togglePasswordVisibility}>
+                <button type="button"  
+                  onClick={togglePasswordVisibility}                
+                >
                   {showPassword ?
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye absolute top-1/2 right-4 -translate-y-1/2" viewBox="0 0 16 16">
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                  </svg>
+                  :
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-eye-slash absolute top-1/2 right-4 -translate-y-1/2" viewBox="0 0 16 16">
                     <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z"/>
                     <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829"/>
                     <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z"/>
                   </svg>
-                  :
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
-                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                  </svg>
                   }
                 </button>
             </div>
-            <button className="bg-[#894A2A] rounded-xl text-white font-semibold py-2 hover:scale-105 duration-300">Login</button>
+            <button className="bg-gradient-to-r from-[#F2C17D] to-[#A44200] rounded-xl text-white font-semibold py-2 hover:scale-105 duration-300">Login</button>
           </form>
 
-          <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
-            <hr className="border-gray-400"/>
+          <div className="mt-6 grid grid-cols-3 items-center text-[#9C785F]">
+            <hr className="border-[#9C785F]"/>
             <p className="text-center text-sm">OR</p>
-            <hr className="border-gray-400"/>
+            <hr className="border-[#9C785F]"/>
           </div>
 
-          <button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#894A2A]">
+          <button className="border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#894A2A]">
             <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
               <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
               <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
@@ -86,7 +88,7 @@ export function Login() {
           <div className="mt-3 text-xs flex justify-between items-center text-[#894A2A]">
             <p>Don't have an account?</p>
             <Link to="/signup">
-              <button type='button' className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Register</button>
+              <button type='button' className="py-2 px-5 border rounded-xl hover:scale-110 duration-300">Register</button>
             </Link>
           </div>
         </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -22,8 +22,8 @@ export function SignUp() {
   }
 
   return (
-    <section className="bg-gray-100 min-h-screen flex items-center justify-center">
-        <div className="bg-white flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+    <section className="bg-gradient-to-r from-[#F8DBA4] to-[#E09082] min-h-screen flex items-center justify-center">
+        <div className="bg-white/50 backdrop-blur-3xl ring-1 ring-black/5 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
             <div className="md:block hidden w-1/2">
                 <img className="rounded-2xl" src="https://file.hstatic.net/1000217270/article/gom_bat_trang_21_604c7f48e7f74a99a1bfc808bff191b2_grande.jpeg"/>
             </div>
@@ -31,58 +31,58 @@ export function SignUp() {
             <div className="md:w-1/2 px-8 md:px-8">
                 <h2 className="text-center font-bold text-2xl text-[#894A2A]">Sign Up</h2>
 
-                <form onSubmit={handleSubmit} className="flex flex-col gap-1">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-2">
                     <div className='flex flex-col gap-1 mt-2'>
                         <label className="font-bold text-sm" htmlFor="username">User name</label>
-                        <input className="p-1 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="text" name="username" required/>
+                        <input className="p-1 rounded-md px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-[#894A2A] focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="text" name="username" required/>
                     </div>
                     <div className='flex flex-col gap-1'>
                         <label className="font-bold text-sm" htmlFor="email">Email</label>
-                        <input className="p-1 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="email" name="username" required/>
+                        <input className="p-1 rounded-md px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-[#894A2A] focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="email" name="username" required/>
                     </div>
                     <div className='flex flex-col gap-1'>
                         <label className="font-bold text-sm" htmlFor="phonenumber">Phone number</label>
-                        <input className="p-1 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="text" name="phonenumber" required/>
+                        <input className="p-1 rounded-md px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-[#894A2A] focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="text" name="phonenumber" required/>
                     </div>
                     
                     <div className="relative flex flex-col gap-1">
                         <label className="font-bold text-sm" htmlFor="password">Password</label>
-                        <input className="p-1 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" 
+                        <input className="p-1 rounded-md px-3 pr-10 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-[#894A2A] focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" 
                             type={showPassword ? "text" : "password"} 
                             name="password" 
                             required minLength={6}
                         />
                             <button type="button" onClick={togglePasswordVisibility}>
                                 {showPassword ?
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash absolute right-3 -translate-y-7.5" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-eye absolute right-3 -translate-y-7.5" viewBox="0 0 16 16">
+                                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                                </svg>
+                                :
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-eye-slash absolute right-3 -translate-y-7.5" viewBox="0 0 16 16">
                                     <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z"/>
                                     <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829"/>
                                     <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z"/>
                                 </svg>
-                                :
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute right-3 -translate-y-7.5" viewBox="0 0 16 16">
-                                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                                </svg>
                                 }
                             </button>
                     </div>
-                    <button className="bg-[#894A2A] rounded-xl text-white font-semibold mt-4 py-2 hover:scale-105 duration-300">Register</button>
+                    <button className="bg-gradient-to-r from-[#F2C17D] to-[#A44200] rounded-xl text-white font-semibold mt-2 py-2 hover:scale-105 duration-300">Register</button>
                 </form>
 
-                <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
-                    <hr className="border-gray-400"/>
+                <div className="mt-6 grid grid-cols-3 items-center text-[#9C785F]">
+                    <hr className="border-[#9C785F]"/>
                     <p className="text-center text-sm">OR</p>
-                    <hr className="border-gray-400"/>
+                    <hr className="border-[#9C785F]"/>
                 </div>
 
                 <div className='flex flex-row gap-1'>
-                    <Link to='/login' className="bg-white border py-2 w-2/5 rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#894A2A]">
+                    <Link to='/login' className="border py-2 w-2/5 rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#894A2A]">
                         <button >
                             Login
                         </button>
                     </Link>
-                    <button className="bg-white border py-2 w-3/5 rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#894A2A]">
+                    <button className="border py-2 w-3/5 rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#894A2A]">
                         <div>
                             <svg className="mr-2.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
                                 <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
