3c2a85c5fbcc42feaf3c391fc492c8069e20568a
ltbanhh <lethanhbach2004@gmail.com>
2025-04-10 09:59:55 +0700

    update login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 export function Login() {
   const navigate = useNavigate()
+  const [showPassword, setShowPassword] = useState(false)
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(prev => !prev)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -24,13 +29,36 @@ export function Login() {
           <p className="text-xs mt-4 text-[#894A2A]">If you are already a member, easily log in</p>
 
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <input className="p-2 mt-8 rounded-md bg-white px-3 py-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="email" name="email" placeholder="Email" required/>
+            <input 
+              className="p-2 mt-8 rounded-md bg-white px-3 py-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" 
+              type="email" 
+              name="email" 
+              placeholder="Email" 
+              required
+            />
             <div className="relative">
-              <input className="p-2 w-full rounded-md bg-white px-3 py-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" type="password" name="password" placeholder="Password" required minLength={6}/>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
-                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-              </svg>
+              <input 
+                className="p-2 w-full rounded-md bg-white px-3 py-2.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#894A2A] sm:text-sm/6" 
+                type={showPassword ? "text" : "password"}
+                name="password" 
+                placeholder="Password" 
+                required 
+                minLength={6}
+              />
+                <button type="button" onClick={togglePasswordVisibility}>
+                  {showPassword ?
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-slash absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
+                    <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z"/>
+                    <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829"/>
+                    <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z"/>
+                  </svg>
+                  :
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                  </svg>
+                  }
+                </button>
             </div>
             <button className="bg-[#894A2A] rounded-xl text-white font-semibold py-2 hover:scale-105 duration-300">Login</button>
           </form>
@@ -64,7 +92,8 @@ export function Login() {
         </div>
 
         <div className="md:block hidden w-2/5">
-          <img className="rounded-2xl" src="https://file.hstatic.net/1000217270/article/gom_bat_trang_21_604c7f48e7f74a99a1bfc808bff191b2_grande.jpeg"/>
+          <img className="rounded-xl" 
+            src="https://file.hstatic.net/1000217270/article/gom_bat_trang_21_604c7f48e7f74a99a1bfc808bff191b2_grande.jpeg"/>
         </div>
       </div>
     </div>
